c9dbff397ba065e97beceb70a2322a17eb2218a7
Joice Blasi <joiceblasi@hotmail.com>
2023-05-02 10:53:31 -0300

    feat: create user and postgresql connection

---
+++ users/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username',
+            'email',
+            'password',
+            'is_admin',
+            'is_seller',
+        ]
