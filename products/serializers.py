9b305cd96e73b719868ca8dbc53b3ea60878f89d
Edmilson <krakadianbill@gmail.com>
2023-05-02 13:40:50 -0300

    feat: initial mask for products added

---
+++ products/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from .models import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(
+        validators=[UniqueValidator(queryset=Product.objects.all())]
+    )
+
+    class Meta:
+        model = Product
+        read_only_fields = ["id"]
+        fields = ["name", "price", "category", "stock", "avaliable"]
