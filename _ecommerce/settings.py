0af1a927b491f54ff7f96f1b72a42e4bdece9aec
Edmilson <krakadianbill@gmail.com>
2023-05-02 14:20:50 -0300

    feat: pagination class added

--- _ecommerce/settings.py
+++ _ecommerce/settings.py
@@ -111,6 +111,10 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
