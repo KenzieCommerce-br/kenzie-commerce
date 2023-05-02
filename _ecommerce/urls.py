1f26582cb9432ea68112eb66bc2411a21326ff40
Edmilson <krakadianbill@gmail.com>
2023-05-02 14:34:24 -0300

    feat: urls added

--- _ecommerce/urls.py
+++ _ecommerce/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+import products
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("users.urls")),
+    path(
+        "api/",
+        include("users.urls", "products.urls"),
+    ),
 ]
--- products/views.py
+++ products/views.py
@@ -1,8 +1,12 @@
-from rest_framework import generics
+from rest_framework.generics import CreateAPIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .permissions import IsVendorOrReadOnly
 from .models import Product
 from .serializers import ProductSerializer
 
 
-class ProductView(generics.CreateAPIView):
+class ProductView(CreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsVendorOrReadOnly]
