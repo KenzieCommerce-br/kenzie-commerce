48d982493c35a620f59f6556fdd909a080ad7d62
franklmarins <franklmarins@gmail.com>
2023-05-04 19:37:19 -0300

    fix: cart refactored

--- carts/models.py
+++ carts/models.py
@@ -2,14 +2,25 @@ from django.db import models
 
 
 class Cart(models.Model):
-    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    products = models.ManyToManyField(
-        "products.Product", through="CartItem", related_name="carts"
-    )
-    total_value = models.DecimalField(max_digits=8, decimal_places=2, null=True)
+    user = models.OneToOneField("users.User", on_delete=models.CASCADE)
+    total_value = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    @property
+    def total_value(self):
+        total = 0
+        for item in self.items.all():
+            total += item.quantity * item.product.price
+        return total
+
+    def update_total_value(self):
+        self.total_value = self.total_value
+        self.save()
 
 
 class CartItem(models.Model):
-    product = models.ForeignKey("products.Product", on_delete=models.CASCADE)
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(
+        "products.Product",
+        on_delete=models.CASCADE,
+    )
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
     quantity = models.PositiveIntegerField(default=1)
--- carts/views.py
+++ carts/views.py
@@ -1,29 +1,54 @@
-from django.shortcuts import get_object_or_404
 from rest_framework.generics import (
-    ListAPIView,
     RetrieveUpdateDestroyAPIView,
-    CreateAPIView,
-    ListCreateAPIView,
-    RetrieveAPIView,
+    GenericAPIView,
 )
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.response import Response
 
 from products.models import Product
-
-
-from .serializers import CartItemSerializer, CartSerializer
-
+from .serializers import CartSerializer
 from .models import Cart, CartItem
+from django.db.models import F
 
 
-class CartView(ListCreateAPIView):
+class CartView(GenericAPIView):
     authentication_classes = [JWTAuthentication]
-    queryset = Cart.objects.all()
+
     serializer_class = CartSerializer
 
-    def perform_create(self, serializer):
-        return serializer.save(user_id=int(self.request.user.id))
+    def get_object(self):
+        user = self.request.user
+        return Cart.objects.get_or_create(user=user)[0]
+
+    def get(self, request):
+        cart = self.get_object()
+        serializer = self.get_serializer(cart)
+        return Response(serializer.data)
+
+    def post(self, request):
+        cart = self.get_object()
+        product_id = request.data.get("product")
+        quantity = request.data.get("quantity", 1)
+        product = Product.objects.get(id=product_id)
+
+        if quantity > product.stock:
+            return Response(
+                {"error": "Não há estoque suficiente para finalizar o pedido."}
+            )
+        getProduct = CartItem.objects.filter(cart=cart, product=product).first()
+        if getProduct:
+            defaults = {"quantity": F("quantity") + quantity}
+        else:
+            defaults = {"quantity": quantity}
+        CartItem.objects.update_or_create(
+            cart=cart,
+            product=product,
+            defaults=defaults,
+        )
+
+        serializer = self.get_serializer(cart)
+
+        return Response(serializer.data)
 
 
 class CartDetailView(RetrieveUpdateDestroyAPIView):
