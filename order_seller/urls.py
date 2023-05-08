55946bd72daaa552cdc9f32fdabaf50a61555e17
srod-douglas <srod.douglas@gmail.com>
2023-05-08 12:09:10 -0300

    feat: add routes pending and finished to sellers

--- order_seller/urls.py
+++ order_seller/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('seller/orders/<int:order_id>/', views.OrderSellerView.as_view()),
+    path('seller/<int:seller_id>/orders/pending/', views.OrderSellerPendingView.as_view()),
+    path('seller/<int:seller_id>/orders/finished/', views.OrderSellerFinishedView.as_view()),
 ]
